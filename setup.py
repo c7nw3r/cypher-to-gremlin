c5825ddc6f9703c52660c33bb09f40c7f5474667
c7nw3r <christian.weber@leftshift.one>
2024-08-26 15:21:22 +0200

    requirements.txt modified, version set to 0.4.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 long_description = Path(__file__).with_name("README.md").read_text()
 
-version = "0.4.0"
+version = "0.4.1"
 
 setup(
     name='cypher-to-gremlin',
