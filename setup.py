4ac0e1f6d3617c59a815106c13e2634ef1439ebd
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-11-12 18:41:01 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages, setup
 
 long_description = Path(__file__).with_name("README.md").read_text()
 
-version = "0.4.12"
+version = "0.4.13"
 
 setup(
     name="cypher-to-gremlin",
